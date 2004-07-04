7d7d7d1a0e45b07eac954637c43ac440a1688eb3
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2004-07-04 09:16:45 +0000

    Merged mpdm-branch-wchar into trunk.

--- stress.c
+++ stress.c
@@ -54,27 +54,28 @@ void test_basic(void)
 	mpdm_v v;
 	mpdm_v w;
 
-	v=MPDM_S("65536");
+	v=MPDM_S(L"65536");
+	mpdm_dump(v);
 	i=mpdm_ival(v);
 
 	_test("i == 65536", (i == 65536));
 	_test("v has MPDM_IVAL", (v->flags & MPDM_IVAL));
 
-	printf("mpdm_string: %s\n", mpdm_string(MPDM_H(0)));
-	printf("mpdm_string: %s\n", mpdm_string(MPDM_H(0)));
+	printf("mpdm_string: %ls\n", mpdm_string(MPDM_H(0)));
+	printf("mpdm_string: %ls\n", mpdm_string(MPDM_H(0)));
 
 	/* partial copies of strings */
-	v=MPDM_LS("this is not America");
-	v=MPDM_NS((char *)v->data + 4, 4);
+	v=MPDM_LS(L"this is not America");
+	v=MPDM_NS((wchar_t *)v->data + 4, 4);
 
-	_test("Partial string values", mpdm_cmp(v, MPDM_LS(" is ")) == 0);
+	_test("Partial string values", mpdm_cmp(v, MPDM_LS(L" is ")) == 0);
 
-	v=MPDM_S("MUAHAHAHA!");
+	v=MPDM_S(L"MUAHAHAHA!");
 	_test("Testing mpdm_clone semantics 1", mpdm_clone(v) == v);
 
 	v=MPDM_A(2);
-	mpdm_aset(v, MPDM_S("evil"), 0);
-	mpdm_aset(v, MPDM_S("dead"), 1);
+	mpdm_aset(v, MPDM_S(L"evil"), 0);
+	mpdm_aset(v, MPDM_S(L"dead"), 1);
 	w=mpdm_clone(v);
 
 	_test("Testing mpdm_clone semantics 2.1", w != v);
@@ -82,8 +83,21 @@ void test_basic(void)
 	v=mpdm_aget(v, 0);
 	_test("Testing mpdm_clone semantics 2.2", v->ref > 1);
 	_test("Testing mpdm_clone semantics 2.3", mpdm_aget(w, 0) == v);
-}
 
+	/* mbs / wcs tests */
+	v=MPDM_MBS("This is (was) a multibyte string");
+	mpdm_dump(v);
+
+	v=MPDM_LS(L"¡España! (non-ASCII string, as wchar_t *)");
+	mpdm_dump(v);
+
+	v=MPDM_MBS("¡España! (non-ASCII string, as ISO-8859-1 char *)");
+	mpdm_dump(v);
+	printf("(Previous value will be NULL if locale doesn't match stress.c encoding)\n");
+
+	v=MPDM_2MBS(L"¡España! (should be correcly seen in any locale)");
+	printf("%s\n", (char *)v->data);
+}
 
 mpdm_v _asort_cb(mpdm_v args)
 {
@@ -104,13 +118,13 @@ void test_array(void)
 	a=MPDM_A(0);
 	_test("a->size == 0", (a->size == 0));
 
-	mpdm_apush(a, MPDM_LS("sunday"));
-	mpdm_apush(a, MPDM_LS("monday"));
-	mpdm_apush(a, MPDM_LS("tuesday"));
-	mpdm_apush(a, MPDM_LS("wednesday"));
-	mpdm_apush(a, MPDM_LS("thursday"));
-	mpdm_apush(a, MPDM_LS("friday"));
-	mpdm_apush(a, MPDM_LS("saturday"));
+	mpdm_apush(a, MPDM_LS(L"sunday"));
+	mpdm_apush(a, MPDM_LS(L"monday"));
+	mpdm_apush(a, MPDM_LS(L"tuesday"));
+	mpdm_apush(a, MPDM_LS(L"wednesday"));
+	mpdm_apush(a, MPDM_LS(L"thursday"));
+	mpdm_apush(a, MPDM_LS(L"friday"));
+	mpdm_apush(a, MPDM_LS(L"saturday"));
 	mpdm_dump(a);
 	_test("a->size == 7", (a->size == 7));
 
@@ -127,17 +141,17 @@ void test_array(void)
 
 	mpdm_asort(a, 1);
 	_test("mpdm_asort() works (1)",
-		mpdm_cmp(mpdm_aget(a,0), MPDM_LS("friday")) == 0);
+		mpdm_cmp(mpdm_aget(a,0), MPDM_LS(L"friday")) == 0);
 	_test("mpdm_asort() works (2)",
-		mpdm_cmp(mpdm_aget(a,6), MPDM_LS("wednesday")) == 0);
+		mpdm_cmp(mpdm_aget(a,6), MPDM_LS(L"wednesday")) == 0);
 
 	/* _asort_cb sorts reversely */
 	mpdm_asort_cb(a, 1, MPDM_X(_asort_cb));
 
 	_test("mpdm_asort_cb() works (1)",
-		mpdm_cmp(mpdm_aget(a,6), MPDM_LS("friday")) == 0);
+		mpdm_cmp(mpdm_aget(a,6), MPDM_LS(L"friday")) == 0);
 	_test("mpdm_asort_cb() works (2)",
-		mpdm_cmp(mpdm_aget(a,0), MPDM_LS("wednesday")) == 0);
+		mpdm_cmp(mpdm_aget(a,0), MPDM_LS(L"wednesday")) == 0);
 
 	n=v->ref;
 	v=mpdm_aget(a, 3);
@@ -185,15 +199,15 @@ void test_hash(void)
 
 	h=MPDM_H(0);
 
-	mpdm_hset(h, MPDM_S("mp"), MPDM_I(6));
-	v=mpdm_hget(h, MPDM_S("mp"));
+	mpdm_hset(h, MPDM_S(L"mp"), MPDM_I(6));
+	v=mpdm_hget(h, MPDM_S(L"mp"));
 
 	_test("hash: v != NULL", (v != NULL));
 	i=mpdm_ival(v);
 	_test("hash: v == 6", (i == 6));
 
-	mpdm_hset(h, MPDM_S("mp2"), MPDM_I(66));
-	v=mpdm_hget(h, MPDM_S("mp2"));
+	mpdm_hset(h, MPDM_S(L"mp2"), MPDM_I(66));
+	v=mpdm_hget(h, MPDM_S(L"mp2"));
 
 	_test("hash: v != NULL", (v != NULL));
 	i=mpdm_ival(v);
@@ -248,41 +262,44 @@ void test_splice(void)
 	mpdm_v w;
 	mpdm_v v;
 
-	w=mpdm_splice(MPDM_LS("I'm agent Johnson"), MPDM_LS("special "), 4, 0);
+	w=mpdm_splice(MPDM_LS(L"I'm agent Johnson"),
+		MPDM_LS(L"special "), 4, 0);
 	_test("splice insertion", 
-		mpdm_cmp(mpdm_aget(w, 0), MPDM_LS("I'm special agent Johnson")) == 0);
+		mpdm_cmp(mpdm_aget(w, 0),
+		MPDM_LS(L"I'm special agent Johnson")) == 0);
 	mpdm_dump(w);
 
-	w=mpdm_splice(MPDM_LS("Life is a shit"), MPDM_LS("cheat"), 10, 4);
+	w=mpdm_splice(MPDM_LS(L"Life is a shit"), MPDM_LS(L"cheat"), 10, 4);
 	_test("splice insertion and deletion (1)", 
-		mpdm_cmp(mpdm_aget(w, 0), MPDM_LS("Life is a cheat")) == 0);
+		mpdm_cmp(mpdm_aget(w, 0), MPDM_LS(L"Life is a cheat")) == 0);
 	_test("splice insertion and deletion (2)", 
-		mpdm_cmp(mpdm_aget(w, 1), MPDM_LS("shit")) == 0);
+		mpdm_cmp(mpdm_aget(w, 1), MPDM_LS(L"shit")) == 0);
 	mpdm_dump(w);
 
-	w=mpdm_splice(MPDM_LS("I'm with dumb"), NULL, 4, 4);
+	w=mpdm_splice(MPDM_LS(L"I'm with dumb"), NULL, 4, 4);
 	_test("splice deletion (1)", 
-		mpdm_cmp(mpdm_aget(w, 0), MPDM_LS("I'm  dumb")) == 0);
+		mpdm_cmp(mpdm_aget(w, 0), MPDM_LS(L"I'm  dumb")) == 0);
 	_test("splice deletion (2)", 
-		mpdm_cmp(mpdm_aget(w, 1), MPDM_LS("with")) == 0);
+		mpdm_cmp(mpdm_aget(w, 1), MPDM_LS(L"with")) == 0);
 	mpdm_dump(w);
 
-	v=MPDM_LS("It doesn't matter");
-	w=mpdm_splice(v, MPDM_LS(" two"), v->size, 0);
+	v=MPDM_LS(L"It doesn't matter");
+	w=mpdm_splice(v, MPDM_LS(L" two"), v->size, 0);
 	_test("splice insertion at the end", 
-		mpdm_cmp(mpdm_aget(w, 0), MPDM_LS("It doesn't matter two")) == 0);
+		mpdm_cmp(mpdm_aget(w, 0),
+		MPDM_LS(L"It doesn't matter two")) == 0);
 	mpdm_dump(w);
 
 	w=mpdm_splice(NULL, NULL, 0, 0);
 	_test("splice with two NULLS", (mpdm_aget(w, 0) == NULL));
 
-	w=mpdm_splice(NULL, MPDM_LS("foo"), 0, 0);
+	w=mpdm_splice(NULL, MPDM_LS(L"foo"), 0, 0);
 	_test("splice with first value NULL",
-		(mpdm_cmp(mpdm_aget(w, 0), MPDM_LS("foo")) == 0));
+		(mpdm_cmp(mpdm_aget(w, 0), MPDM_LS(L"foo")) == 0));
 
-	w=mpdm_splice(MPDM_LS("foo"), NULL, 0, 0);
+	w=mpdm_splice(MPDM_LS(L"foo"), NULL, 0, 0);
 	_test("splice with second value NULL",
-		(mpdm_cmp(mpdm_aget(w, 0), MPDM_LS("foo")) == 0));
+		(mpdm_cmp(mpdm_aget(w, 0), MPDM_LS(L"foo")) == 0));
 }
 
 
@@ -292,27 +309,27 @@ void test_asplit(void)
 
 	printf("mpdm_asplit test\n\n");
 
-	w=mpdm_asplit(MPDM_S("."), MPDM_S("four.elems.in.string"));
+	w=mpdm_asplit(MPDM_S(L"."), MPDM_S(L"four.elems.in.string"));
 	mpdm_dump(w);
 	_test("4 elems: ", (w->size == 4));
 
-	w=mpdm_asplit(MPDM_S("."), MPDM_S("unseparated string"));
+	w=mpdm_asplit(MPDM_S(L"."), MPDM_S(L"unseparated string"));
 	mpdm_dump(w);
 	_test("1 elem: ", (w->size == 1));
 
-	w=mpdm_asplit(MPDM_S("."), MPDM_S(".dot.at start"));
+	w=mpdm_asplit(MPDM_S(L"."), MPDM_S(L".dot.at start"));
 	mpdm_dump(w);
 	_test("3 elems: ", (w->size == 3));
 
-	w=mpdm_asplit(MPDM_S("."), MPDM_S("dot.at end."));
+	w=mpdm_asplit(MPDM_S(L"."), MPDM_S(L"dot.at end."));
 	mpdm_dump(w);
 	_test("3 elems: ", (w->size == 3));
 
-	w=mpdm_asplit(MPDM_S("."), MPDM_S("three...dots (two empty elements)"));
+	w=mpdm_asplit(MPDM_S(L"."), MPDM_S(L"three...dots (two empty elements)"));
 	mpdm_dump(w);
 	_test("4 elems: ", (w->size == 4));
 
-	w=mpdm_asplit(MPDM_S("."), MPDM_S("."));
+	w=mpdm_asplit(MPDM_S(L"."), MPDM_S(L"."));
 	mpdm_dump(w);
 	_test("2 elems: ", (w->size == 2));
 }
@@ -327,27 +344,27 @@ void test_ajoin(void)
 	printf("mpdm_ajoin test\n\n");
 
 	/* separator */
-	s=MPDM_LS("--");
+	s=MPDM_LS(L"--");
 
 	w=MPDM_A(1);
-	mpdm_aset(w, MPDM_S("ce"), 0);
+	mpdm_aset(w, MPDM_S(L"ce"), 0);
 
 	v=mpdm_ajoin(NULL, w);
-	_test("1 elem, no separator", (mpdm_cmp(v, MPDM_LS("ce")) == 0));
+	_test("1 elem, no separator", (mpdm_cmp(v, MPDM_LS(L"ce")) == 0));
 
 	v=mpdm_ajoin(s, w);
-	_test("1 elem, '--' separator", (mpdm_cmp(v, MPDM_LS("ce")) == 0));
+	_test("1 elem, '--' separator", (mpdm_cmp(v, MPDM_LS(L"ce")) == 0));
 
-	mpdm_apush(w, MPDM_LS("n'est"));
+	mpdm_apush(w, MPDM_LS(L"n'est"));
 	v=mpdm_ajoin(s, w);
-	_test("2 elems, '--' separator", (mpdm_cmp(v, MPDM_LS("ce--n'est")) == 0));
+	_test("2 elems, '--' separator", (mpdm_cmp(v, MPDM_LS(L"ce--n'est")) == 0));
 
-	mpdm_apush(w, MPDM_LS("pas"));
+	mpdm_apush(w, MPDM_LS(L"pas"));
 	v=mpdm_ajoin(s, w);
-	_test("3 elems, '--' separator", (mpdm_cmp(v, MPDM_LS("ce--n'est--pas")) == 0));
+	_test("3 elems, '--' separator", (mpdm_cmp(v, MPDM_LS(L"ce--n'est--pas")) == 0));
 
 	v=mpdm_ajoin(NULL, w);
-	_test("3 elems, no separator", (mpdm_cmp(v, MPDM_LS("cen'estpas")) == 0));
+	_test("3 elems, no separator", (mpdm_cmp(v, MPDM_LS(L"cen'estpas")) == 0));
 }
 
 
@@ -358,17 +375,17 @@ void test_sym(void)
 
 	printf("mpdm_sset / mpdm_sget tests\n\n");
 
-	mpdm_sset(NULL, MPDM_LS("mp"), MPDM_H(7));
-	mpdm_sset(NULL, MPDM_LS("mp.config"), MPDM_H(7));
-	mpdm_sset(NULL, MPDM_LS("mp.config.auto_indent"), MPDM_I(16384));
-	mpdm_sset(NULL, MPDM_LS("mp.config.use_regex"), MPDM_I(1357));
-	mpdm_sset(NULL, MPDM_LS("mp.config.gtk_font_face"), MPDM_LS("profontwindows"));
-	mpdm_sset(NULL, MPDM_LS("mp.lines"), MPDM_A(2));
-	mpdm_sset(NULL, MPDM_LS("mp.lines.0"), MPDM_LS("First post!"));
-	mpdm_sset(NULL, MPDM_LS("mp.lines.1"), MPDM_LS("Second post!"));
+	mpdm_sset(NULL, MPDM_LS(L"mp"), MPDM_H(7));
+	mpdm_sset(NULL, MPDM_LS(L"mp.config"), MPDM_H(7));
+	mpdm_sset(NULL, MPDM_LS(L"mp.config.auto_indent"), MPDM_I(16384));
+	mpdm_sset(NULL, MPDM_LS(L"mp.config.use_regex"), MPDM_I(1357));
+	mpdm_sset(NULL, MPDM_LS(L"mp.config.gtk_font_face"), MPDM_LS(L"profontwindows"));
+	mpdm_sset(NULL, MPDM_LS(L"mp.lines"), MPDM_A(2));
+	mpdm_sset(NULL, MPDM_LS(L"mp.lines.0"), MPDM_LS(L"First post!"));
+	mpdm_sset(NULL, MPDM_LS(L"mp.lines.1"), MPDM_LS(L"Second post!"));
 	mpdm_dump(mpdm_root());
 
-	v=mpdm_sget(NULL, MPDM_LS("mp.config.auto_indent"));
+	v=mpdm_sget(NULL, MPDM_LS(L"mp.config.auto_indent"));
 	i=mpdm_ival(v);
 
 	_test("auto_indent == 16384", (i == 16384));
@@ -383,21 +400,21 @@ void test_file(void)
 	mpdm_v v;
 	mpdm_v w;
 
-	f=mpdm_open(MPDM_LS("test.txt"), MPDM_LS("w"));
+	f=mpdm_open(MPDM_LS(L"test.txt"), MPDM_LS(L"w"));
 	_test("Create test.txt", f != NULL);
 
-	mpdm_write(f, MPDM_LS("0"));
-	mpdm_write(f, MPDM_LS("1"));
+	mpdm_write(f, MPDM_LS(L"0"));
+	mpdm_write(f, MPDM_LS(L"1"));
 
 	/* test an array */
 	v=MPDM_A(4);
-	mpdm_aset(v, MPDM_LS("2.0"), 0);
-	mpdm_aset(v, MPDM_LS("2.1"), 1);
-	mpdm_aset(v, MPDM_LS("2.2"), 2);
+	mpdm_aset(v, MPDM_LS(L"2.0"), 0);
+	mpdm_aset(v, MPDM_LS(L"2.1"), 1);
+	mpdm_aset(v, MPDM_LS(L"2.2"), 2);
 
 	w=MPDM_A(2);
-	mpdm_aset(w, MPDM_LS("3.0.0"), 0);
-	mpdm_aset(w, MPDM_LS("3.0.1"), 1);
+	mpdm_aset(w, MPDM_LS(L"3.0.0"), 0);
+	mpdm_aset(w, MPDM_LS(L"3.0.1"), 1);
 	mpdm_aset(v, w, 3);
 
 	mpdm_write(f, v);
@@ -407,24 +424,24 @@ void test_file(void)
 
 	mpdm_close(f);
 
-	f=mpdm_open(MPDM_LS("test.txt"), MPDM_LS("r"));
+	f=mpdm_open(MPDM_LS(L"test.txt"), MPDM_LS(L"r"));
 
-	_test("test written file 0", mpdm_cmp(mpdm_read(f), MPDM_LS("0")) == 0);
-	_test("test written file 1", mpdm_cmp(mpdm_read(f), MPDM_LS("1")) == 0);
-	_test("test written file 2.0", mpdm_cmp(mpdm_read(f), MPDM_LS("2.0")) == 0);
-	_test("test written file 2.1", mpdm_cmp(mpdm_read(f), MPDM_LS("2.1")) == 0);
-	_test("test written file 2.2", mpdm_cmp(mpdm_read(f), MPDM_LS("2.2")) == 0);
-	_test("test written file 3.0.1", mpdm_cmp(mpdm_read(f), MPDM_LS("3.0.0")) == 0);
-	_test("test written file 3.0.2", mpdm_cmp(mpdm_read(f), MPDM_LS("3.0.1")) == 0);
+	_test("test written file 0", mpdm_cmp(mpdm_read(f), MPDM_LS(L"0")) == 0);
+	_test("test written file 1", mpdm_cmp(mpdm_read(f), MPDM_LS(L"1")) == 0);
+	_test("test written file 2.0", mpdm_cmp(mpdm_read(f), MPDM_LS(L"2.0")) == 0);
+	_test("test written file 2.1", mpdm_cmp(mpdm_read(f), MPDM_LS(L"2.1")) == 0);
+	_test("test written file 2.2", mpdm_cmp(mpdm_read(f), MPDM_LS(L"2.2")) == 0);
+	_test("test written file 3.0.1", mpdm_cmp(mpdm_read(f), MPDM_LS(L"3.0.0")) == 0);
+	_test("test written file 3.0.2", mpdm_cmp(mpdm_read(f), MPDM_LS(L"3.0.1")) == 0);
 	v=mpdm_read(f);
 	mpdm_dump(v);
 
 	mpdm_close(f);
 
-	mpdm_unlink(MPDM_LS("test.txt"));
-	_test("unlink", mpdm_open(MPDM_LS("test.txt"), MPDM_LS("r")) == NULL);
+	mpdm_unlink(MPDM_LS(L"test.txt"));
+	_test("unlink", mpdm_open(MPDM_LS(L"test.txt"), MPDM_LS(L"r")) == NULL);
 
-	v=mpdm_glob(MPDM_LS("*"));
+	v=mpdm_glob(MPDM_LS(L"*"));
 	mpdm_dump(v);
 }
 
@@ -434,67 +451,71 @@ void test_regex(void)
 	mpdm_v v;
 	mpdm_v w;
 
-	v=mpdm_regex(MPDM_LS("/[0-9]+/"), MPDM_LS("123456"), 0);
+	v=mpdm_regex(MPDM_LS(L"/[0-9]+/"), MPDM_LS(L"123456"), 0);
 	_test("regex 0", v != NULL);
 
-	v=mpdm_regex(MPDM_LS("/[0-9]+/"), MPDM_I(65536), 0);
+	v=mpdm_regex(MPDM_LS(L"/[0-9]+/"), MPDM_I(65536), 0);
 	_test("regex 1", v != NULL);
 
-	v=mpdm_regex(MPDM_LS("/^[0-9]+$/"), MPDM_LS("12345678"), 0);
+	v=mpdm_regex(MPDM_LS(L"/^[0-9]+$/"), MPDM_LS(L"12345678"), 0);
 	_test("regex 2", v != NULL);
 
-	v=mpdm_regex(MPDM_LS("/^[0-9]+$/"), MPDM_I(1), 0);
+	v=mpdm_regex(MPDM_LS(L"/^[0-9]+$/"), MPDM_I(1), 0);
 	_test("regex 3", v != NULL);
 
-	v=mpdm_regex(MPDM_LS("/^[0-9]+$/"), MPDM_LS("A12345-678"), 0);
+	v=mpdm_regex(MPDM_LS(L"/^[0-9]+$/"), MPDM_LS(L"A12345-678"), 0);
 	_test("regex 4", v == NULL);
 
-	w=MPDM_LS("Hell street, 666");
-	v=mpdm_regex(MPDM_LS("/[0-9]+/"), w, 0);
+	w=MPDM_LS(L"Hell street, 666");
+	v=mpdm_regex(MPDM_LS(L"/[0-9]+/"), w, 0);
 	_test("regex 5", v != NULL);
 
 	mpdm_dump(v);
 
-	v=mpdm_regex(MPDM_LS("/regex/i"), MPDM_LS("CASE-INSENSITIVE REGEX"), 0);
+	v=mpdm_regex(MPDM_LS(L"/regex/i"), MPDM_LS(L"CASE-INSENSITIVE REGEX"), 0);
 	_test("regex 6", v != NULL);
 
 	/* sregex */
 
-	v=mpdm_sregex(MPDM_LS("/A/"),MPDM_LS("change all A to A"),
-		MPDM_LS("E"),0);
-	_test("sregex 0", mpdm_cmp(v, MPDM_LS("change all E to A")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/A/"),MPDM_LS(L"change all A to A"),
+		MPDM_LS(L"E"),0);
+	_test("sregex 0", mpdm_cmp(v, MPDM_LS(L"change all E to A")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/A/g"),MPDM_LS("change all A to A"),
-		MPDM_LS("E"),0);
-	_test("sregex 1", mpdm_cmp(v, MPDM_LS("change all E to E")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/A/g"),MPDM_LS(L"change all A to A"),
+		MPDM_LS(L"E"),0);
+	_test("sregex 1", mpdm_cmp(v, MPDM_LS(L"change all E to E")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/A+/g"),MPDM_LS("change all AAAAAA to E"),
-		MPDM_LS("E"),0);
-	_test("sregex 2", mpdm_cmp(v, MPDM_LS("change all E to E")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/A+/g"),MPDM_LS(L"change all AAAAAA to E"),
+		MPDM_LS(L"E"),0);
+	_test("sregex 2", mpdm_cmp(v, MPDM_LS(L"change all E to E")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/A+/g"),MPDM_LS("change all A A A A A A to E"),
-		MPDM_LS("E"),0);
-	_test("sregex 3", mpdm_cmp(v, MPDM_LS("change all E E E E E E to E")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/A+/g"),MPDM_LS(L"change all A A A A A A to E"),
+		MPDM_LS(L"E"),0);
+	_test("sregex 3", mpdm_cmp(v, MPDM_LS(L"change all E E E E E E to E")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/[0-9]+/g"),MPDM_LS("1, 20, 333, 40 all are numbers"),
-		MPDM_LS("numbers"),0);
-	_test("sregex 4", mpdm_cmp(v, MPDM_LS("numbers, numbers, numbers, numbers all are numbers")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/A+/g"),MPDM_LS(L"change all AAA A AA AAAAA A AAA to E"),
+		MPDM_LS(L"E"),0);
+	_test("sregex 3.2", mpdm_cmp(v, MPDM_LS(L"change all E E E E E E to E")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/[a-zA-Z_]+/g"),MPDM_LS("regex, mpdm_regex, TexMex"),
-		MPDM_LS("sex"),0);
-	_test("sregex 5", mpdm_cmp(v, MPDM_LS("sex, sex, sex")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/[0-9]+/g"),MPDM_LS(L"1, 20, 333, 40 all are numbers"),
+		MPDM_LS(L"numbers"),0);
+	_test("sregex 4", mpdm_cmp(v, MPDM_LS(L"numbers, numbers, numbers, numbers all are numbers")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/[a-zA-Z]+/g"),MPDM_LS("regex, mpdm_regex, TexMex"),
+	v=mpdm_sregex(MPDM_LS(L"/[a-zA-Z_]+/g"),MPDM_LS(L"regex, mpdm_regex, TexMex"),
+		MPDM_LS(L"sex"),0);
+	_test("sregex 5", mpdm_cmp(v, MPDM_LS(L"sex, sex, sex")) == 0);
+
+	v=mpdm_sregex(MPDM_LS(L"/[a-zA-Z]+/g"),MPDM_LS(L"regex, mpdm_regex, TexMex"),
 		NULL,0);
-	_test("sregex 6", mpdm_cmp(v, MPDM_LS(", _, ")) == 0);
+	_test("sregex 6", mpdm_cmp(v, MPDM_LS(L", _, ")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/\\\\/g"),MPDM_LS("\\MSDOS\\style\\path"),
-		MPDM_LS("/"),0);
-	_test("sregex 7", mpdm_cmp(v, MPDM_LS("/MSDOS/style/path")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/\\\\/g"),MPDM_LS(L"\\MSDOS\\style\\path"),
+		MPDM_LS(L"/"),0);
+	_test("sregex 7", mpdm_cmp(v, MPDM_LS(L"/MSDOS/style/path")) == 0);
 
-	v=mpdm_sregex(MPDM_LS("/regex/gi"),MPDM_LS("regex, Regex, REGEX"),
-		MPDM_LS("sex"),0);
-	_test("sregex 8", mpdm_cmp(v, MPDM_LS("sex, sex, sex")) == 0);
+	v=mpdm_sregex(MPDM_LS(L"/regex/gi"),MPDM_LS(L"regex, Regex, REGEX"),
+		MPDM_LS(L"sex"),0);
+	_test("sregex 8", mpdm_cmp(v, MPDM_LS(L"sex, sex, sex")) == 0);
 
 }
 
@@ -549,7 +570,7 @@ static mpdm_v _calculator(mpdm_v c, mpdm_v args)
 		/* gets next value */
 		v=mpdm_adel(a, 0);
 
-		switch(*(char *)o->data)
+		switch(*(wchar_t *)o->data)
 		{
 		case '+': t += mpdm_ival(v); break;
 		case '-': t -= mpdm_ival(v); break;
@@ -588,9 +609,9 @@ void test_exec(void)
 
 	/* calculator 'script' */
 	p=MPDM_A(0);
-	mpdm_apush(p, MPDM_LS("+"));
-	mpdm_apush(p, MPDM_LS("-"));
-	mpdm_apush(p, MPDM_LS("+"));
+	mpdm_apush(p, MPDM_LS(L"+"));
+	mpdm_apush(p, MPDM_LS(L"-"));
+	mpdm_apush(p, MPDM_LS(L"+"));
 
 	/* the value */
 	x=MPDM_A(2);
@@ -603,9 +624,9 @@ void test_exec(void)
 
 	/* another 'script', different operations with the same values */
 	p=MPDM_A(0);
-	mpdm_apush(p, MPDM_LS("*"));
-	mpdm_apush(p, MPDM_LS("/"));
-	mpdm_apush(p, MPDM_LS("+"));
+	mpdm_apush(p, MPDM_LS(L"*"));
+	mpdm_apush(p, MPDM_LS(L"/"));
+	mpdm_apush(p, MPDM_LS(L"+"));
 
 	mpdm_aset(x, p, 1);
 
