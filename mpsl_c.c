de2fd81983d26137a2e35c22bfcffa1ea932d2a7
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2005-02-02 16:01:56 +0000

    More macro definitions to simplify the virtual machine code.

--- mpsl_c.c
+++ mpsl_c.c
@@ -312,6 +312,11 @@ mpdm_v mpsl_get_symbol(mpdm_v s)
 #define M2 M(2)
 #define M3 M(3)
 
+#define RM1 mpdm_rval(M(1))
+#define RM2 mpdm_rval(M(2))
+#define IM1 mpdm_ival(M(1))
+#define IM2 mpdm_ival(M(2))
+
 static mpdm_v _O_multi(mpdm_v c, mpdm_v a) { M1; return(M2); }
 static mpdm_v _O_literal(mpdm_v c, mpdm_v a) { return(mpdm_clone(C1)); }
 static mpdm_v _O_symval(mpdm_v c, mpdm_v a) { return(mpsl_get_symbol(M1)); }
@@ -320,27 +325,27 @@ static mpdm_v _O_exec(mpdm_v c, mpdm_v a) { return(mpdm_exec(M1, M2)); }
 static mpdm_v _O_if(mpdm_v c, mpdm_v a) { return(mpsl_is_true(M1) ? M2 : M3); }
 static mpdm_v _O_while(mpdm_v c, mpdm_v a) { while(mpsl_is_true(M1)) M2; return(NULL); }
 static mpdm_v _O_local(mpdm_v c, mpdm_v a) { mpsl_local_set_symbols(M1, NULL); return(NULL); }
-static mpdm_v _O_uminus(mpdm_v c, mpdm_v a) { return(MPDM_R(-mpdm_rval(M1))); }
-static mpdm_v _O_add(mpdm_v c, mpdm_v a) { return(MPDM_R(mpdm_rval(M1) + mpdm_rval(M2))); }
-static mpdm_v _O_sub(mpdm_v c, mpdm_v a) { return(MPDM_R(mpdm_rval(M1) - mpdm_rval(M2))); }
-static mpdm_v _O_mul(mpdm_v c, mpdm_v a) { return(MPDM_R(mpdm_rval(M1) * mpdm_rval(M2))); }
-static mpdm_v _O_div(mpdm_v c, mpdm_v a) { return(MPDM_R(mpdm_rval(M1) / mpdm_rval(M2))); }
-static mpdm_v _O_mod(mpdm_v c, mpdm_v a) { return(MPDM_I(mpdm_ival(M1) % mpdm_ival(M2))); }
+static mpdm_v _O_uminus(mpdm_v c, mpdm_v a) { return(MPDM_R(-RM1)); }
+static mpdm_v _O_add(mpdm_v c, mpdm_v a) { return(MPDM_R(RM1 + RM2)); }
+static mpdm_v _O_sub(mpdm_v c, mpdm_v a) { return(MPDM_R(RM1 - RM2)); }
+static mpdm_v _O_mul(mpdm_v c, mpdm_v a) { return(MPDM_R(RM1 * RM2)); }
+static mpdm_v _O_div(mpdm_v c, mpdm_v a) { return(MPDM_R(RM1 / RM2)); }
+static mpdm_v _O_mod(mpdm_v c, mpdm_v a) { return(MPDM_I(IM1 % IM2)); }
 static mpdm_v _O_not(mpdm_v c, mpdm_v a) { return(mpsl_boolean(! mpsl_is_true(M1))); }
 static mpdm_v _O_and(mpdm_v c, mpdm_v a) { mpdm_v r=M1; return(mpsl_is_true(r) ? M2 : r); }
 static mpdm_v _O_or(mpdm_v c, mpdm_v a) { mpdm_v r=M1; return(mpsl_is_true(r) ? r : M2); }
-static mpdm_v _O_numlt(mpdm_v c, mpdm_v a) { return(mpsl_boolean(mpdm_rval(M1) < mpdm_rval(M2))); }
-static mpdm_v _O_numle(mpdm_v c, mpdm_v a) { return(mpsl_boolean(mpdm_rval(M1) <= mpdm_rval(M2))); }
-static mpdm_v _O_numgt(mpdm_v c, mpdm_v a) { return(mpsl_boolean(mpdm_rval(M1) > mpdm_rval(M2))); }
-static mpdm_v _O_numge(mpdm_v c, mpdm_v a) { return(mpsl_boolean(mpdm_rval(M1) >= mpdm_rval(M2))); }
+static mpdm_v _O_numlt(mpdm_v c, mpdm_v a) { return(mpsl_boolean(RM1 < RM2)); }
+static mpdm_v _O_numle(mpdm_v c, mpdm_v a) { return(mpsl_boolean(RM1 <= RM2)); }
+static mpdm_v _O_numgt(mpdm_v c, mpdm_v a) { return(mpsl_boolean(RM1 > RM2)); }
+static mpdm_v _O_numge(mpdm_v c, mpdm_v a) { return(mpsl_boolean(RM1 >= RM2)); }
 static mpdm_v _O_strcat(mpdm_v c, mpdm_v a) { return(mpdm_strcat(M1, M2)); }
 static mpdm_v _O_streq(mpdm_v c, mpdm_v a) { return(mpsl_boolean(mpdm_cmp(M1, M2) == 0)); }
 static mpdm_v _O_immpinc(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) + 1))); }
 static mpdm_v _O_immpdec(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) - 1))); }
-static mpdm_v _O_immadd(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) + mpdm_rval(M2)))); }
-static mpdm_v _O_immsub(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) - mpdm_rval(M2)))); }
-static mpdm_v _O_immmul(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) * mpdm_rval(M2)))); }
-static mpdm_v _O_immdiv(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) / mpdm_rval(M2)))); }
+static mpdm_v _O_immadd(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) + RM2))); }
+static mpdm_v _O_immsub(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) - RM2))); }
+static mpdm_v _O_immmul(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) * RM2))); }
+static mpdm_v _O_immdiv(mpdm_v c, mpdm_v a) { mpdm_v s=M1; return(mpsl_set_symbol(s, MPDM_R(mpdm_rval(mpsl_get_symbol(s)) / RM2))); }
 
 static mpdm_v _O_list(mpdm_v c, mpdm_v a)
 /* build list from instructions */
@@ -421,7 +426,7 @@ static mpdm_v _O_immmath(mpdm_v c, mpdm_v a)
 
 	/* gets the (optional) second value */
 	if(mpdm_size(c) > 2)
-		r2=mpdm_rval(M2);
+		r2=RM2;
 
 	switch(op)
 	{
