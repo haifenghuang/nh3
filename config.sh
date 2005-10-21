651daa19ff175466e09b9e1c0ef9f9ca3ccf2689
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2005-10-21 13:55:14 +0000

    mpdm's config.ldflags is appended after configuring mpdm (not before).

--- config.sh
+++ config.sh
@@ -117,12 +117,12 @@ else
 	exit 1
 fi
 
-cat $MPDM/config.ldflags >> config.ldflags
-
 # If mpdm is not configured, do it
 if [ ! -f $MPDM/Makefile ] ; then
 	( cd $MPDM ; ./config.sh )
 fi
+
+cat $MPDM/config.ldflags >> config.ldflags
 
 #########################################################
 
