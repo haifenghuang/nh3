f488d360ad13310266b1466bfe268c25e9085222
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2005-12-15 07:22:54 +0000

    Configuration options are propagated to mpdm's config.sh, and mpsl.exe is made the target if under win32.

--- config.sh
+++ config.sh
@@ -2,6 +2,8 @@
 
 # Configuration shell script
 
+TARGET=mpsl
+
 # gets program version
 VERSION=`cut -f2 -d\" VERSION`
 
@@ -14,9 +16,11 @@ while [ $# -gt 0 ] ; do
 	case $1 in
 	--without-win32)	WITHOUT_WIN32=1 ;;
 	--without-unix-glob)	WITHOUT_UNIX_GLOB=1 ;;
-	--without-regex)	WITHOUT_REGEX=1 ;;
 	--with-included-regex)	WITH_INCLUDED_REGEX=1 ;;
 	--with-pcre)		WITH_PCRE=1 ;;
+	--without-gettext)	WITHOUT_GETTEXT=1 ;;
+	--without-iconv)	WITHOUT_ICONV=1 ;;
+	--without-wcwidth)	WITHOUT_WCWIDTH=1 ;;
 	--help)			CONFIG_HELP=1 ;;
 
 	--prefix)		PREFIX=$2 ; shift ;;
@@ -34,6 +38,9 @@ if [ "$CONFIG_HELP" = "1" ] ; then
 	echo "--without-unix-glob   Disable glob.h usage (use workaround)."
 	echo "--with-included-regex Use included regex code (gnu_regex.c)."
 	echo "--with-pcre           Enable PCRE library detection."
+	echo "--without-gettext     Disable gettext usage."
+	echo "--without-iconv       Disable iconv usage."
+	echo "--without-wcwidth     Disable system wcwidth() (use workaround)."
 
 	echo
 	echo "Environment variables:"
@@ -119,10 +126,21 @@ fi
 
 # If mpdm is not configured, do it
 if [ ! -f $MPDM/Makefile ] ; then
-	( cd $MPDM ; ./config.sh )
+	CONF_ARGS="--prefix=$PREFIX"
+	[ "$WITHOUT_WIN32" = 1 ] && CONF_ARGS="$CONF_ARGS --without-win32"
+	[ "$WITHOUT_UNIX_GLOB" = 1 ] && CONF_ARGS="$CONF_ARGS --without-unix-glob"
+	[ "$WITH_INCLUDED_REGEX" = 1 ] && CONF_ARGS="$CONF_ARGS --with-included-regex"
+	[ "$WITH_PCRE" = 1 ] && CONF_ARGS="$CONF_ARGS --with-pcre"
+	[ "$WITHOUT_GETTEXT" = 1 ] && CONF_ARGS="$CONF_ARGS --without-gettext"
+	[ "$WITHOUT_ICONV" = 1 ] && CONF_ARGS="$CONF_ARGS --without-iconv"
+	[ "$WITHOUT_WCWIDTH" = 1 ] && CONF_ARGS="$CONF_ARGS --without-wcwidth"
+	( echo ; cd $MPDM ; ./config.sh $CONF_ARGS ; echo )
 fi
 
 cat $MPDM/config.ldflags >> config.ldflags
+
+# if win32, the interpreter is called mpsl.exe
+grep CONFOPT_WIN32 ${MPDM}/config.h >/dev/null && TARGET=mpsl.exe
 
 #########################################################
 
@@ -131,6 +149,7 @@ cat $MPDM/config.ldflags >> config.ldflags
 echo "MPDM=$MPDM" >> makefile.opts
 echo "VERSION=$VERSION" >> makefile.opts
 echo "PREFIX=$PREFIX" >> makefile.opts
+echo "TARGET=$TARGET" >> makefile.opts
 echo >> makefile.opts
 
 cat makefile.opts makefile.in makefile.depend > Makefile
