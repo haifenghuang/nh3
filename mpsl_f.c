16d06da7a7d0263acd9521623044926eda999170
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2005-12-26 18:35:39 +0000

    New function compile(), that compiles a string of mpsl code() and returns an executable value.

--- mpsl_f.c
+++ mpsl_f.c
@@ -86,6 +86,7 @@ static mpdm_t F_encoding(mpdm_t a) { return(MPDM_I(mpdm_encoding(A0))); }
 static mpdm_t F_regex(mpdm_t a) { return(mpdm_regex(A0,A1,IA2)); }
 static mpdm_t F_sregex(mpdm_t a) { return(mpdm_sregex(A0,A1,A2,IA3)); }
 
+static mpdm_t F_compile(mpdm_t a) { return(mpsl_compile(A0)); }
 static mpdm_t F_load(mpdm_t a) { return(mpdm_exec(mpsl_compile_file(A0), NULL)); }
 
 static mpdm_t F_print(mpdm_t a)
@@ -137,6 +138,7 @@ static struct
 	{ L"encoding",	F_encoding },
 	{ L"regex",	F_regex },
 	{ L"sregex",	F_sregex },
+	{ L"compile",	F_compile },
 	{ L"load",	F_load },
 	{ L"print",	F_print },
 	{ NULL,		NULL }
