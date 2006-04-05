6140ae32e8554caf4c1fdba6c7b479ea529c9790
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2006-04-05 15:19:43 +0000

    New function eval() (Closes: #1078).

--- mpsl_f.c
+++ mpsl_f.c
@@ -86,8 +86,17 @@ static mpdm_t F_encoding(mpdm_t a) { return(MPDM_I(mpdm_encoding(A0))); }
 static mpdm_t F_regex(mpdm_t a) { return(mpdm_regex(A0,A1,IA2)); }
 static mpdm_t F_sregex(mpdm_t a) { return(mpdm_sregex(A0,A1,A2,IA3)); }
 
-static mpdm_t F_compile(mpdm_t a) { return(mpsl_compile(A0)); }
 static mpdm_t F_load(mpdm_t a) { return(mpdm_exec(mpsl_compile_file(A0), NULL)); }
+
+static mpdm_t F_eval(mpdm_t a)
+{
+	mpdm_t c;
+
+	a = mpdm_clone(a);
+	c = mpdm_adel(a, 0);
+
+	return(mpsl_eval(c, a));
+}
 
 static mpdm_t F_print(mpdm_t a)
 {
@@ -138,8 +147,8 @@ static struct
 	{ L"encoding",	F_encoding },
 	{ L"regex",	F_regex },
 	{ L"sregex",	F_sregex },
-	{ L"compile",	F_compile },
 	{ L"load",	F_load },
+	{ L"eval",	F_eval },
 	{ L"print",	F_print },
 	{ NULL,		NULL }
 };
