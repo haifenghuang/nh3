e35a118ac4360dc347c2d2e1925aefbac64b168e
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2006-05-13 11:50:45 +0000

    Added a stress test (that fails) for the returning value of a while().

--- stress.c
+++ stress.c
@@ -520,6 +520,12 @@ void test_mpsl3(void)
 	v = do_test_exec(v, NULL);
 	do_test("return 2", mpdm_ival(v) == 500);
 
+	v = do_test_mpsl("sub func() { while(1 == 1) { return(6); 24; } 12; }");
+	v = do_test_exec(v, NULL);
+	v = do_test_mpsl("a=func();");
+	v = do_test_exec(v, NULL);
+	do_test("return 3", mpdm_ival(v) == 6);
+
 	v = do_test_mpsl("a=1; while(a < 10) { a++; } a;");
 	v = do_test_exec(v, NULL);
 	do_test("while", mpdm_ival(v) == 10);
