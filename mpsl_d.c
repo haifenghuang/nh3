54a8dd8ea46b2c1c3ae708e7e3e086be95b6c872
Angel Ortega <angel@triptico.com>
2009-04-06 17:08:50 +0200

    Use mpdm_pokews() instead of mpdm_poke() wherever possible.

--- mpsl_d.c
+++ mpsl_d.c
@@ -38,32 +38,32 @@ static wchar_t *dump_string(const mpdm_t v, wchar_t *ptr, int *size)
 {
 	wchar_t *iptr = mpdm_string(v);
 
-	ptr = mpdm_poke(ptr, size, L"\"", 1, sizeof(wchar_t));
+	ptr = mpdm_pokews(ptr, size, L"\"");
 
 	while (*iptr != L'\0') {
 		switch (*iptr) {
 		case '"':
-			ptr = mpdm_poke(ptr, size, L"\\\"", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\\\"");
 			break;
 
 		case '\'':
-			ptr = mpdm_poke(ptr, size, L"\\'", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\\'");
 			break;
 
 		case '\r':
-			ptr = mpdm_poke(ptr, size, L"\\r", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\\r");
 			break;
 
 		case '\n':
-			ptr = mpdm_poke(ptr, size, L"\\n", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\\n");
 			break;
 
 		case '\t':
-			ptr = mpdm_poke(ptr, size, L"\\t", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\\t");
 			break;
 
 		case '\\':
-			ptr = mpdm_poke(ptr, size, L"\\\\", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\\\\");
 			break;
 
 		default:
@@ -73,7 +73,7 @@ static wchar_t *dump_string(const mpdm_t v, wchar_t *ptr, int *size)
 		iptr++;
 	}
 
-	ptr = mpdm_poke(ptr, size, L"\"", 1, sizeof(wchar_t));
+	ptr = mpdm_pokews(ptr, size, L"\"");
 
 	return ptr;
 }
@@ -86,65 +86,65 @@ wchar_t *mpsl_dump_1(const mpdm_t v, int l, wchar_t *ptr, int *size)
 
 	/* indent */
 	for (n = 0; n < l; n++)
-		ptr = mpdm_poke(ptr, size, L"  ", 2, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L"  ");
 
 	if (v == NULL)
-		ptr = mpdm_poke(ptr, size, L"NULL", 4, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L"NULL");
 	else
 	if (MPDM_IS_EXEC(v)) {
-		ptr = mpdm_poke(ptr, size, L"sub { 1; }", 10, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L"sub { 1; }");
 	}
 	else
 	if (MPDM_IS_HASH(v)) {
 		mpdm_t a = mpdm_keys(v);
 
-		ptr = mpdm_poke(ptr, size, L"{\n", 2, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L"{\n");
 
 		for (n = 0; n < mpdm_size(a); n++) {
 			mpdm_t k = mpdm_aget(a, n);
 			mpdm_t w = mpdm_hget(v, k);
 
 			ptr = mpsl_dump_1(k, l + 1, ptr, size);
-			ptr = mpdm_poke(ptr, size, L" => ", 4, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L" => ");
 			ptr = mpsl_dump_1(w, l + 1, ptr, size);
 
 			if (n < mpdm_size(a) - 1)
-				ptr = mpdm_poke(ptr, size, L",", 1, sizeof(wchar_t));
+				ptr = mpdm_pokews(ptr, size, L",");
 
-			ptr = mpdm_poke(ptr, size, L"\n", 1, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\n");
 		}
 
 		/* re-indent */
 		for (n = 0; n < l; n++)
-			ptr = mpdm_poke(ptr, size, L"  ", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"  ");
 
-		ptr = mpdm_poke(ptr, size, L"}", 1, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L"}");
 	}
 	else
 	if (MPDM_IS_ARRAY(v)) {
-		ptr = mpdm_poke(ptr, size, L"[\n", 2, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L"[\n");
 
 		for (n = 0; n < mpdm_size(v); n++) {
 			ptr = mpsl_dump_1(mpdm_aget(v, n), l + 1, ptr, size);
 
 			if (n < mpdm_size(v) - 1)
-				ptr = mpdm_poke(ptr, size, L",", 1, sizeof(wchar_t));
+				ptr = mpdm_pokews(ptr, size, L",");
 
-			ptr = mpdm_poke(ptr, size, L"\n", 1, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"\n");
 		}
 
 		/* re-indent */
 		for (n = 0; n < l; n++)
-			ptr = mpdm_poke(ptr, size, L"  ", 2, sizeof(wchar_t));
+			ptr = mpdm_pokews(ptr, size, L"  ");
 
-		ptr = mpdm_poke(ptr, size, L"]", 1, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L"]");
 	}
 	else
 	if (MPDM_IS_STRING(v))
 		ptr = dump_string(v, ptr, size);
 
 	if (l == 0)
-		ptr = mpdm_poke(ptr, size, L";\n", 2, sizeof(wchar_t));
+		ptr = mpdm_pokews(ptr, size, L";\n");
 
 	return ptr;
 }
