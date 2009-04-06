47dca5cb61ab0efbe109f848c745c8c4db0e6ace
Angel Ortega <angel@triptico.com>
2009-04-06 11:58:52 +0200

    New file mpsl_d.c.

---
+++ mpsl_d.c
@@ -0,0 +1,33 @@
+/*
+
+    MPSL - Minimum Profit Scripting Language
+    Copyright (C) 2003/2007 Angel Ortega <angel@triptico.com>
+
+    mpsl_d.c - Minimum Profit Scripting Language debugging functions
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+    http://www.triptico.com
+
+*/
+
+#include "config.h"
+
+/** code **/
+
+wchar_t *mpdm_dump_1(const mpdm_t v, int l, wchar_t *ptr, int *size)
+{
+	return ptr;
+}
