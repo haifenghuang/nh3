2fedae3ddec30db4f2826bbfe2bed5ce389d326d
Angel Ortega <angel@triptico.com>
2011-04-05 09:34:03 +0200

    Do not create _ unless needed (Closes: #1131).

--- mpsl_c.c
+++ mpsl_c.c
@@ -134,11 +134,13 @@ static void set_local_symbols(mpdm_t s, mpdm_t v, mpdm_t l)
             for (n = 0; n < mpdm_size(s); n++)
                 mpdm_hset(h, mpdm_aget(s, n), mpdm_aget(v, n));
 
-            /* store the rest of arguments into _ */
-            a = mpdm_hset_s(h, L"_", MPDM_A(0));
+            if (n < mpdm_size(v)) {
+                /* store the rest of arguments into _ */
+                a = mpdm_hset_s(h, L"_", MPDM_A(0));
 
-            for (; n < mpdm_size(v); n++)
-                mpdm_push(a, mpdm_aget(v, n));
+                for (; n < mpdm_size(v); n++)
+                    mpdm_push(a, mpdm_aget(v, n));
+            }
         }
         else
             mpdm_hset(h, s, v);
