73ba807c3e0b8fc39853e6996e05fce9a94bebd1
Angel Ortega <angel@triptico.com>
2012-10-03 15:11:32 +0200

    Rewritten mpsl_disasm().

--- mpsl_c.c
+++ mpsl_c.c
@@ -1350,38 +1350,83 @@ mpdm_t mpsl_compile(mpdm_t src)
 }
 
 
+static struct _mpsl_asm {
+    mpsl_op_t   op;
+    wchar_t     *str;
+    int         argc;
+} mpsl_asm[] = {
+    { OP_EOP,   L"EOP", 0 },
+    { OP_LIT,   L"LIT", 1 },
+    { OP_NUL,   L"NUL", 0 },
+    { OP_ARR,   L"ARR", 0 },
+    { OP_HSH,   L"HSH", 0 },
+    { OP_POP,   L"POP", 0 },
+    { OP_SWP,   L"SWP", 0 },
+    { OP_DUP,   L"DUP", 0 },
+    { OP_DP2,   L"DP2", 0 },
+    { OP_DPN,   L"DPN", 1 },
+    { OP_GET,   L"GET", 0 },
+    { OP_SET,   L"SET", 0 },
+    { OP_STI,   L"STI", 0 },
+    { OP_APU,   L"APU", 0 },
+    { OP_TBL,   L"TBL", 0 },
+    { OP_TPU,   L"TPU", 0 },
+    { OP_TPO,   L"TPO", 0 },
+    { OP_TLT,   L"TLT", 0 },
+    { OP_THS,   L"THS", 0 },
+    { OP_CAL,   L"CAL", 0 },
+    { OP_RET,   L"RET", 0 },
+    { OP_ARG,   L"ARG", 0 },
+    { OP_JMP,   L"JMP", 1 },
+    { OP_JF,    L"JF",  1 },
+    { OP_AND,   L"AND", 0 },
+    { OP_OR,    L"OR",  0 },
+    { OP_XOR,   L"XOR", 0 },
+    { OP_SHL,   L"SHL", 0 },
+    { OP_SHR,   L"SHR", 0 },
+    { OP_ADD,   L"ADD", 0 },
+    { OP_SUB,   L"SUB", 0 },
+    { OP_MUL,   L"MUL", 0 },
+    { OP_DIV,   L"DIV", 0 },
+    { OP_MOD,   L"MOD", 0 },
+    { OP_NOT,   L"NOT", 0 },
+    { OP_EQ,    L"EQ",  0 },
+    { OP_GT,    L"GT",  0 },
+    { OP_GE,    L"GE",  0 },
+    { OP_LT,    L"LT",  0 },
+    { OP_LE,    L"LE",  0 },
+    { OP_REM,   L"REM", 1 },
+    { OP_CAT,   L"CAT", 0 },
+    { OP_ITE,   L"ITE", 1 },
+    { OP_FMT,   L"FMT", 0 },
+    { OP_LNI,   L"LNI", 1 },
+    { -1,       NULL,   0 }
+};
+
 void mpsl_disasm(mpdm_t prg)
 {
     int n;
-    static char *ops[] = {
-        "EOP",
-        "LIT", "NUL", "ARR", "HSH",
-        "POP", "SWP", "DUP", "DP2", "DPN",
-        "GET", "SET", "STI", "APU", "TBL",
-        "TPU", "TPO", "TLT", "THS",
-        "CAL", "RET", "ARG",
-        "JMP", "JF",
-        "AND", "OR", "XOR", "SHL", "SHR",
-        "ADD", "SUB", "MUL", "DIV", "MOD",
-        "NOT", "EQ", "GT", "GE", "LT", "LE",
-        "REM", "CAT", "ITE", "FMT", "LNI"
-    };
 
     mpdm_ref(prg);
 
     for (n = 0; n < mpdm_size(prg); n++) {
         mpsl_op_t i = mpdm_ival(mpdm_aget(prg, n));
+        struct _mpsl_asm *a;
+        int m = 0;
 
-        printf("%4d: ", n);
-        printf("%s", ops[i]);
+        while ((a = &mpsl_asm[m++]) && a->op != -1 && a->op != i);
 
-        if (i == OP_LIT || i == OP_REM)
-            printf(" \"%ls\"", mpdm_string(mpdm_aget(prg, ++n)));
-        if (i == OP_JMP || i == OP_JF ||
-            i == OP_ITE || i == OP_DPN || i == OP_LNI)
-            printf(" %d", mpdm_ival(mpdm_aget(prg, ++n)));
+        if (a->op == -1)
+            printf("Error: opcode id #%d not found\n", i);
+        else {
+            printf("%4d: ", n);
+            printf("%ls",   a->str);
 
-        printf("\n");
+            if (a->argc)
+                printf(" \"%ls\"", mpdm_string(mpdm_aget(prg, ++n)));
+
+            printf("\n");
+        }
     }
 
     mpdm_unref(prg);
